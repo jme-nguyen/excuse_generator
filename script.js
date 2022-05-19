afa802891f2677db97bfb58e156a4c730cfa24f2
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2022-05-19 19:14:58 +1000

    Initialised variables to hold parts of sentence we are going to generate

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
 const sentenceStructures = {
-
+    beginning: [],
+    relation: [],
+    excuse: [],
+    createExcuse() {
+        beginningPhrase = this.beginning(Math.floor(Math.random()*2))
+    }
 };
