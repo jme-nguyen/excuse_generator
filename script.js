daccc6745f5baa004144a509d9157f87947dfebb
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2022-05-19 21:04:12 +1000

    Fixed bug in function and added strings to arrays

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 'use strict';
 
 const beginning = ['I','My'];
-const relation = ['Dog','Sister','Mother','Father','Girlfriend','Grandpa','Grandma',];
-const excuse = [];
+const relation = ['dog','sister','mother','father','girlfriend','grandpa','grandma',];
+const relationExcuse = ['has covid','has gotten sick','has died'];
+const excuse = ['have covid','am sick','have a headache'];
 
 const createExcuse = () => {
     const startOfSentence = beginning[Math.floor(Math.random() * beginning.length)];
-    const relationOfSentence = relation[Math.floor(Math.round() * relation.length)];
-    const excuseOfSentence = excuse[Math.floor(Math.random() * excuse.length)];
+    const relationOfSentence = relation[Math.floor(Math.random() * relation.length)];
     
-    if(beginning === 'My'){
+    if(startOfSentence === 'My'){
+        const excuseOfSentence = relationExcuse[Math.floor(Math.random() * relationExcuse.length)];
         return `${startOfSentence} ${relationOfSentence} ${excuseOfSentence}.`;
     }
 
+    const excuseOfSentence = excuse[Math.floor(Math.random() * excuse.length)];
     return `${startOfSentence} ${excuseOfSentence}.`;
 };
